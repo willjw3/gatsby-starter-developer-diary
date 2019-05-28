385649355ce3da096e52b576fdea35ebb8dba1cc
Will Ward <will.j.ward3@gmail.com>
2019-05-28 11:10:20 +0900

    Added 3 dependencies react-icons, react-bootstrap and bootstrap. Social links in header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,53 +1,11 @@
-// import { Link } from "gatsby"
-// import PropTypes from "prop-types"
-// import React from "react"
-
-// const Header = ({ siteTitle }) => (
-//   <header
-//     style={{
-//       background: `rebeccapurple`,
-//       marginBottom: `1.45rem`,
-//     }}
-//   >
-//     <div
-//       style={{
-//         margin: `0 auto`,
-//         maxWidth: 960,
-//         padding: `1.45rem 1.0875rem`,
-//       }}
-//     >
-//       <h1 style={{ margin: 0 }}>
-//         <Link
-//           to="/"
-//           style={{
-//             color: `white`,
-//             textDecoration: `none`,
-//           }}
-//         >
-//           {siteTitle}
-//         </Link>
-//       </h1>
-//     </div>
-//   </header>
-// )
-
-// Header.propTypes = {
-//   siteTitle: PropTypes.string,
-// }
-
-// Header.defaultProps = {
-//   siteTitle: ``,
-// }
-
-// export default Header
-
+import React from "react"
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
 
-//import MobileSocialLinks from "./MobileSocialLinks"
+
+import MobileSocialLinks from "./MobileSocialLinks"
 //import MobileBio from "./MobileBio"
-//import SocialLinks from "./SocialLinks"
+import SocialLinks from "./SocialLinks"
 import "./header.css"
 
 const Header = ({ siteTitle, tagline, author, contacts }) => {
@@ -76,9 +34,9 @@ const Header = ({ siteTitle, tagline, author, contacts }) => {
             {siteTitle}
           </Link>
         </h1>
-        
+        <SocialLinks contacts={contacts} />
       </div>
-      
+      <MobileSocialLinks contacts={contacts} />
     </header>
   )
 }
