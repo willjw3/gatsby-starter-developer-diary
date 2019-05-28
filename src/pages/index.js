3bfc930943832f6bf81b42e5dc7267ae14d54510
Will Ward <will.j.ward3@gmail.com>
2019-05-28 10:03:55 +0900

    Changed displayed date format to month/year only -graphql query

--- src/pages/index.js
+++ src/pages/index.js
@@ -62,7 +62,7 @@ export const query = graphql`
                  id
                  frontmatter {
                    title
-                   date(formatString: "DD MMMM, YYYY")
+                   date(formatString: "MMMM, YYYY")
                  }
                  fields {
                    slug
