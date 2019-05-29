3e146ebfecdcc7f41873bf8ed7d1b0db9530b21c
Will Ward <will.j.ward3@gmail.com>
2019-05-29 23:53:20 +0900

    Updated 404 page

--- src/components/image.js
+++ src/components/image.js
@@ -17,7 +17,7 @@ const Image = () => (
   <StaticQuery
     query={graphql`
       query {
-        placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+        placeholderImage: file(relativePath: { eq: "gatsby-icon.png" }) {
           childImageSharp {
             fluid(maxWidth: 300) {
               ...GatsbyImageSharpFluid
--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,9 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>Page Not Found</h1>
+    <p><i>Just lightin&apos; out for the territories,
+huh? Brother, I been there.</i></p>
   </Layout>
 )
 
